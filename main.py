5c2a435e7bee05f712562cdf0022f6cfef9ed275
tanxiao <tanxiao@deepvision-tech.com>
2023-10-30 17:20:22 -0400

    basic framework

---
+++ main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+The main program maintaining the video capturing and displaying window
+"""
+import argparse
+from types import ModuleType
+
+import cv2
+
+from video import VideoCapture
+
+
+class Loop:
+    def __init__(self, cap: VideoCapture, title="video"):
+        self.cap = cap
+        self.title = title
+
+    def run(self):
+        while True:
+            if not self.loop():
+                break
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+    def loop(self) -> bool:
+        ret, frame = self.cap.read()
+        if not ret:
+            print(f"cannot read {self.cap}")
+            return False
+
+        # show frame:
+        cv2.imshow(self.title, frame)
+        if cv2.waitKey(1) == ord('q'):
+            return False
+        return True
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("settings", default=None, help="a python module containing everything")
+
+    args = parser.parse_args()
+    with open(args.settings, "r") as file:
+        content = file.read()
+    settings = ModuleType("settings")
+    exec(content, settings.__dict__)
+
+    loop = Loop(
+        cap=VideoCapture(settings.device, api_preference=settings.api_preference)
+    )
+    loop.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ video.py
@@ -0,0 +1,51 @@
+import cv2
+
+
+class VideoCapture:
+    def __init__(self, device=None, api_preference=cv2.CAP_ANY):
+        if not isinstance(device, int):
+            device = str(device)
+        self.device = device
+        self.cap = cv2.VideoCapture()
+
+        if device is not None:
+            self.open(device, api_preference)
+
+    def __repr__(self):
+        return f"VideoCapture('{self.device}')"
+
+    def open(self, device, api_reference):
+        self.cap.open(device, api_reference)
+
+    def release(self):
+        self.cap.release()
+
+    def set(self, width=None, height=None, fps=None, brightness=None, contrast=None, saturation=None,
+            hue=None, exposure=None):
+
+        relation_map = {
+            cv2.CAP_PROP_FRAME_WIDTH: width,
+            cv2.CAP_PROP_FRAME_HEIGHT: height,
+            cv2.CAP_PROP_FPS: fps,
+            cv2.CAP_PROP_BRIGHTNESS: brightness,
+            cv2.CAP_PROP_CONTRAST: contrast,
+            cv2.CAP_PROP_SATURATION: saturation,
+            cv2.CAP_PROP_HUE: hue,
+            cv2.CAP_PROP_EXPOSURE: exposure,
+        }
+
+        for key, value in relation_map.items():
+            if value is not None:
+                self.cap.set(key, value)
+
+    @property
+    def opened(self):
+        return self.cap.isOpened()
+
+    def read(self):
+        """
+        read a frame
+
+        :return: (bool, np.ndarray)
+        """
+        return self.cap.read()
